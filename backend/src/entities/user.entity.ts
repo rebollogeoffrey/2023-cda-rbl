8c2bcb9ad9b70df2ab0a61f1d1dba3ed63e79ade
Gamilliam <brnlcecile@gmail.com>
2023-09-13 12:35:27 +0200

    User entity created

---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  BASIC = 'basic',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.BASIC,
+  })
+  role: UserRole;
+
+  @Column({
+    type: 'varchar',
+    length: 80,
+    unique: false,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: false,
+  })
+  password: string;
+}
