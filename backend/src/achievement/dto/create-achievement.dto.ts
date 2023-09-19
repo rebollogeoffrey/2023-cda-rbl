37e4d822442584020fc017860899a1b5d10a7801
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-09-19 16:38:15 +0200

    achievement and game dto nearly done

--- backend/src/achievement/dto/create-achievement.dto.ts
+++ backend/src/achievement/dto/create-achievement.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAchievementDto {}
+import { IsString, IsBoolean, Length } from 'class-validator';
+export class CreateAchievementDto {
+  @Length(10, 255)
+  @IsString()
+  readonly title: string;
+
+  @Length(10, 255)
+  @IsString()
+  readonly description: string;
+
+  @Length(10, 255)
+  @IsString()
+  readonly url_image: string;
+
+  @IsBoolean()
+  readonly is_owned: boolean;
+}
--- backend/src/game/dto/create-game.dto.ts
+++ backend/src/game/dto/create-game.dto.ts
@@ -1,1 +1,11 @@
-export class CreateGameDto {}
+import { IsString, Length } from 'class-validator';
+
+export class CreateGameDto {
+  @Length(2, 80)
+  @IsString()
+  name: string;
+
+  @Length(10, 255)
+  @IsString()
+  description: string;
+}
