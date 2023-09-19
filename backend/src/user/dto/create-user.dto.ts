c07a7006666421f08672d3290c41fae45df0de92
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-09-19 16:23:35 +0200

    user dto done

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,38 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  Length,
+  IsStrongPassword,
+  IsString,
+  IsEnum,
+} from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsEnum(UserRole)
+  readonly role: UserRole;
+
+  @Length(1, 80)
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Length(10, 255)
+  readonly email: string;
+
+  @IsNotEmpty()
+  @Length(5, 255)
+  @IsStrongPassword({
+    minLength: 5,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  readonly password: string;
+
+  @IsString()
+  readonly url_image: string;
+}
