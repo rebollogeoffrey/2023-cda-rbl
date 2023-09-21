03b14df1841647a1527b1d50a21c420a0c05d522
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-09-21 11:15:02 +0200

    added the new values for the new entities

--- backend/src/mock_data/1694681037113-mock_data.ts
+++ backend/src/mock_data/1694681037113-mock_data.ts
@@ -21,71 +21,95 @@ import { valuesItem } from './item';
 import { valuesCharacter } from './character';
 import { valuesMonster } from './monster';
 // import { valuesCategory } from './category';
+// import { valuesHistoric } from './historic';
+// import { valuesStatistic } from './statistic';
 
 export class MockData1694681037113 {
   public async up(): Promise<void> {
-    // MockData for User
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(User)
-      .values(valuesUser)
-      .execute();
+    const dataArray = [
+      { table: User, value: valuesUser },
+      { table: Achievement, value: valuesAchievement },
+      { table: Effect, value: valuesEffect },
+      { table: Requirement, value: valuesRequirement },
+      { table: Game, value: valuesGame },
+      { table: Item, value: valuesItem },
+      { table: Character, value: valuesCharacter },
+      { table: Monster, value: valuesMonster },
+      // { table: Category, value: valuesCategory },
+      // { table: Historic, value: valuesHistoric },
+      // { table: Statitic, value: valuesStatistic },
+    ];
 
-    // MockData for Achievement
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Achievement)
-      .values(valuesAchievement)
-      .execute();
+    dataArray.map((data) => {
+      return mockConfig
+        .createQueryBuilder()
+        .insert()
+        .into(data.table)
+        .values(data.value)
+        .execute();
+    });
+    // // MockData for User
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(User)
+    //   .values(valuesUser)
+    //   .execute();
 
-    // MockData for Effect
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Effect)
-      .values(valuesEffect)
-      .execute();
+    // // MockData for Achievement
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Achievement)
+    //   .values(valuesAchievement)
+    //   .execute();
 
-    // MockData for Requirement
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Requirement)
-      .values(valuesRequirement)
-      .execute();
+    // // MockData for Effect
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Effect)
+    //   .values(valuesEffect)
+    //   .execute();
 
-    // MockData for Game
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Game)
-      .values(valuesGame)
-      .execute();
+    // // MockData for Requirement
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Requirement)
+    //   .values(valuesRequirement)
+    //   .execute();
 
-    // MockData for Item
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Item)
-      .values(valuesItem)
-      .execute();
+    // // MockData for Game
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Game)
+    //   .values(valuesGame)
+    //   .execute();
 
-    // MockData for Character
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Character)
-      .values(valuesCharacter)
-      .execute();
+    // // MockData for Item
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Item)
+    //   .values(valuesItem)
+    //   .execute();
 
-    // MockData for Monster
-    await mockConfig
-      .createQueryBuilder()
-      .insert()
-      .into(Monster)
-      .values(valuesMonster)
-      .execute();
+    // // MockData for Character
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Character)
+    //   .values(valuesCharacter)
+    //   .execute();
+
+    // // MockData for Monster
+    // await mockConfig
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Monster)
+    //   .values(valuesMonster)
+    //   .execute();
   }
 }
---
+++ backend/src/mock_data/historic.ts
@@ -0,0 +1,50 @@
+export const valuesHistoric = [
+  {
+    id: '1efe30ae-479b-41f5-9fff-b74ae63f0c85',
+    nb_killed: 2,
+    category_id: '7547fd73-8535-4eb2-b4b3-cd460c4d04d3',
+    statistic_id: '8090798b-f2b2-48e1-b623-9a8c6bb5680d',
+  },
+  {
+    id: 'd9c87439-e68e-4200-ac9c-5d9c2db70858',
+    nb_killed: 8,
+    statistic_id: '48852b46-82de-447a-b7ed-288cfbb5bd4f',
+    category_id: '757007e7-3a67-4467-a429-c6ed6e1f35c2',
+  },
+  {
+    id: '2f04a44d-d7aa-4614-ac75-0207710d0a40',
+    nb_killed: 13,
+    statistic_id: '8090798b-f2b2-48e1-b623-9a8c6bb5680d',
+    category_id: 'e938ddc4-907e-4f9d-a29f-2829042a0a73',
+  },
+  {
+    id: 'b974de2e-685f-44e9-9e5d-8d1f6c33156b',
+    nb_killed: 4,
+    category_id: '48e82833-caff-4b83-b991-52203a632f56',
+    statistic_id: '48852b46-82de-447a-b7ed-288cfbb5bd4f',
+  },
+  {
+    id: '4ff54cc5-d74c-41b3-9fc0-3466349d8b2f',
+    nb_killed: 4,
+    statistic_id: '8090798b-f2b2-48e1-b623-9a8c6bb5680d',
+    category_id: '8ff5c2ed-569e-4404-a1af-035ca2463ed9',
+  },
+  {
+    id: '6518107c-b83d-4279-ab92-d829ce8e4f8f',
+    nb_killed: 7,
+    statistic_id: '48852b46-82de-447a-b7ed-288cfbb5bd4f',
+    category_id: '3906e013-68af-4ff2-a45d-c20c3e68b16c',
+  },
+  {
+    id: '79392cdc-01d7-455f-9788-7d755c77e742',
+    nb_killed: 10,
+    statistic_id: '48852b46-82de-447a-b7ed-288cfbb5bd4f',
+    category_id: 'a46cf62a-57de-4e0c-b19a-279c8c9d71c6',
+  },
+  {
+    id: 'cbd5de3b-a369-49f4-a41d-4bfebe8af8b8',
+    nb_killed: 11,
+    statistic_id: '48852b46-82de-447a-b7ed-288cfbb5bd4f',
+    category_id: '861a6072-bd19-40c5-bc15-b282101f9b6c',
+  },
+];
