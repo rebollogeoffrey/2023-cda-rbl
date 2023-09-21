060646dace1de2096a70172076c78e7aa0c0b048
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-09-21 14:17:49 +0200

    added nestjs/passport

--- backend/src/config/mock.config.ts
+++ backend/src/config/mock.config.ts
@@ -1,8 +1,15 @@
 // Base
 import { DataSource } from 'typeorm';
-import { databaseConfig } from './database.config';
+import 'dotenv/config';
 
 export default new DataSource({
-  ...databaseConfig,
+  type: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '3306'),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB_TEST,
+  entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
+  synchronize: false,
   migrations: [__dirname + '/../mock_data/*-mock_data.ts'],
 });
